55d17c83ed803200ce1a999aa9c8c04ceff0cd10
Aditi Kelvekar <aditi.kelvekar@education.nsw.gov.au>
2021-10-22 10:01:54 +1100

    Finished forLoop

---
+++ 05forLoop.py
@@ -0,0 +1,9 @@
+print(' -' + '-'*(46) + '-')
+print("|                                                |")
+print("|    05forLoop                                   |")
+print("|    Name : Aditi                                |")
+print("|    Version : 01                                |")
+print("|                                                |")
+print(' -' + '-'*(46) + '-')
+for x in range(500):
+  print(x) 
