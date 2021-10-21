994c4a148e15fe8c5e5bdfbfe4cedfa469ec092e
Aditi Kelvekar <aditi.kelvekar@education.nsw.gov.au>
2021-10-21 12:02:14 +1100

    fixed messed up hello world

--- 01HelloWorld.py
+++ 01HelloWorld.py
@@ -1,6 +1,6 @@
 print(' -' + '-'*(46) + '-')
 print("|                                                |")
-print("|    01GoodbyeWorld                              |")
+print("|    01HelloWorld                                |")
 print("|    Name : Aditi                                |")
 print("|    Version : 01                                |")
 print("|                                                |")
