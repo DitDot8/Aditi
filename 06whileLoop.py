a9b7e5dbc0e71c3dcea6a1b5ed5d40b9f0fed126
Aditi Kelvekar <aditi.kelvekar@education.nsw.gov.au>
2021-10-27 12:14:08 +1100

    Finished number 6

---
+++ 06whileLoop.py
@@ -0,0 +1,19 @@
+print(' -' + '-'*(46) + '-')
+print("|                                                |")
+print("|    06whileLoop                                 |")
+print("|    Name : Aditi                                |")
+print("|    Version : 01                                |")
+print("|                                                |")
+print(' -' + '-'*(46) + '-')
+i = 0
+while i == 0:
+    name = input("What is the name of this subject ")
+    if name == "IST":
+        print("")
+        print("")
+        print(" Congratulations!!")
+        print("")
+        print("")
+        break
+    else:
+        print("Not Correct - try again")
